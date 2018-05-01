ab95b597d7c2a737c09da7e1e395a9b01cc8a49e
Alejandro <92.alejandrogarcia@gmail.com>
2018-05-01 13:23:55 +0200

    working version of tab system

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ import InputField from './LockedInput.js';
 import NextLetter from './NextLetter.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);  // basklassens konstruktor
+    this.state = {
+      tabContent: <Login />
+    };
+  }
   render() {
     return (
       <div className="App">
@@ -15,10 +21,14 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <main>
-          < Tabs />
-          <Login/>
-          <InputField/>
-          <NextLetter/>
+          <div id="tabMenu">
+            <div id="nav">
+              <div className="tab" id="tab1" onClick={() => { this.setState({ tabContent: <Login /> }) }}>Tab 1</div>
+              <div className="tab" id="tab2" onClick={() => { this.setState({ tabContent: <InputField /> }) }}>Tab 2</div>
+              <div className="tab" id="tab3" onClick={() => { this.setState({ tabContent: <NextLetter /> }) }}>Tab 3</div>
+            </div>
+            <Tabs thisTab={this.state.tabContent} />
+          </div>
         </main>
       </div>
     );
--- src/Tabs.js
+++ src/Tabs.js
@@ -1,49 +1,29 @@
 import React, { Component } from 'react';
+import Login from './Login.js'
+import InputField from './LockedInput.js'
+import NextLetter from './NextLetter.js'
 
 class Tabs extends Component {
-  constructor(props) {
+  /*constructor(props) {
 		super(props);  // basklassens konstruktor
 		this.state = {
       currentTab : 'tab1',
+      tabContent: props.thisTab
       };
     this.showTab = this.showTab.bind(this);
     this.changeTab = this.changeTab.bind(this);
-
-	}
+  }*/
   render() {
-    let tabContent;
-    switch (this.state.currentTab){
-      case 'tab1':
-        tabContent = 'In tab 1 you would normally find information about Uncle David`s Recipees website. However, the website is not functional yet.'
-        break
-      case 'tab2':
-        tabContent = 'In tab 2 you would normally find information about Google and its news. However, the website is not functional yet.'
-        break
-      case 'tab3':
-        tabContent = 'In tab 3 you would normally find information about the Weather app AccuWeather. However, the website is not functional yet.'
-        break
-        default:
-        tabContent = 'In tab 1 you would normally find information about Uncle David`s Recipees website. However, the website is not functional yet.'
-        
-      }
     return (
-          <div id="tabMenu">
-            <div id="nav">
-              <div className="tab" id="tab1" onClick={this.changeTab}>Tab 1</div>
-              <div className="tab" id="tab2" onClick={this.changeTab}>Tab 2</div>
-              <div className="tab" id="tab3" onClick={this.changeTab}>Tab 3</div>
-            </div>
-            <div id="content">{tabContent}</div>
-            <button type="button" onClick={this.showTab}>CLICK ME</button>
-          </div>
+      <div id="content">{this.props.thisTab}</div>
     );
   }
-  changeTab = event => {
+  /*changeTab = event => {
     this.setState({currentTab: event.target.id});
    }
    showTab() {
     console.log(this.state.currentTab);
-   }
+   }*/
   /*changeTab(event) {
     this.setState({currentTab: event.target.id});
     console.log(this.state.currentTab)
