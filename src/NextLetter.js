5991cc17de01f099a893d437d7e72cc0ed188bb0
Alejandro <92.alejandrogarcia@gmail.com>
2018-05-02 10:59:31 +0200

    fix bugs on tab 2 and add clear function and original text display on bottom

--- src/NextLetter.js
+++ src/NextLetter.js
@@ -10,23 +10,24 @@ class NextLetter extends React.Component {
     this.state = { value: '', number: 1, original: '' };
   }
   newNumber(event) {
-    if (event.target.value === null || event.target.value === undefined || event.target.value === 0) {
+    if (event.target.value === null || event.target.value === undefined || event.target.value === ''|| event.target.value === 1) {
       this.setState({ number: 1 });
     } else {
       this.setState({ number: Number(event.target.value) })
     }
   }
   clear() {
-    this.setState({ value: '' })
+    this.setState({ value: '', original: '' })
   }
   checkInput(event) {
     let originalText = this.state.original + event.key;
     this.setState({original: originalText})
     let x = event.charCode;
     let xz;
-    if (this.state.number === 1) {
+    if (this.state.number === null || this.state.number === undefined || this.state.number === 0 || this.state.number === '') {
       if (x === 122) { xz = 97 } //maybe good to add ternery operator here
       else if (x === 90) { xz = 65 }
+      else { xz = x + this.state.number }
     } else {
         if (x + this.state.number <= 126) {
           xz = x + this.state.number
@@ -45,7 +46,7 @@ class NextLetter extends React.Component {
       <div id="nextletterDiv">
         <p>CAESAR CIPHER YOUR TEXT</p>
         <label>Enter distance factor for complementary characters:</label>
-        <input id="caesarnumber" type="number" onChange={this.newNumber} />
+        <input id="caesarnumber" type="number" onChange={this.newNumber}/>
         <label>Type here for ciphering:</label>
         <textarea id="caesar" value={this.state.value} onKeyDown={this.checkDelete} onKeyPress={this.checkInput} /> <button id="clearBtn" onClick={this.clear}>Clear</button>
         <p>Original text: {this.state.original}</p>
